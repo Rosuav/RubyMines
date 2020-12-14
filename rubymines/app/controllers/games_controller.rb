66cf629ed7561e59adbe40e4c7be46f38219cfbb
Chris Angelico <rosuav@gmail.com>
2020-12-14 13:13:35 +1100

    Return the locations of mines

--- rubymines/app/controllers/games_controller.rb
+++ rubymines/app/controllers/games_controller.rb
@@ -1,12 +1,18 @@
 class GamesController < ApplicationController
 	def fetch
-		game = Game.find_by(width:params[:width], height:params[:height], mines:params[:mines])
+		game = Game.find_by(height:params[:height], width:params[:width], mines:params[:mines])
 		# If there's no game matching that, this will return null. The front end should establish
 		# a websocket to await an available game.
 		if not game
 			render json: nil
 			return
 		end
+		game = game.as_json
+		game["mines"] = []
+		# TODO: Simplify this down. I just want to fetch all the [x,y] pairs for this game.
+		Mine.where(game_id: game["id"]).each do | mine | game["mines"].append([mine["x"], mine["y"]]) end
+		# assert game["mines"].length == Integer(params[:mines])
+		# TODO: Delete this game and its mines (not done in development b/c easier to not recreate)
 		render json: game.to_json
 	end
 end
--- rubymines/config/routes.rb
+++ rubymines/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-	get "/game/:width/:height/:mines", to: "games#fetch"
+	get "/game/:height/:width/:mines", to: "games#fetch"
 	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
