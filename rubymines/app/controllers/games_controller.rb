d6292e4eadbdaf8c825f2f0bfa4170604cf2b72b
Chris Angelico <rosuav@gmail.com>
2020-12-14 12:37:27 +1100

    Explicitly render null and bail if none found

--- rubymines/app/controllers/games_controller.rb
+++ rubymines/app/controllers/games_controller.rb
@@ -3,6 +3,10 @@ class GamesController < ApplicationController
 		game = Game.find_by(width:params[:width], height:params[:height], mines:params[:mines])
 		# If there's no game matching that, this will return null. The front end should establish
 		# a websocket to await an available game.
+		if not game
+			render json: nil
+			return
+		end
 		render json: game.to_json
 	end
 end
