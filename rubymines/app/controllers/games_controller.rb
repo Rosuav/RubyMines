8a2f1e4434b4c0eaaa2dbe6f2b159798f53756f4
Chris Angelico <rosuav@gmail.com>
2020-12-14 12:12:09 +1100

    Create a static HTML landing page

--- rubymines/app/controllers/games_controller.rb
+++ rubymines/app/controllers/games_controller.rb
@@ -1,4 +1,8 @@
 class GamesController < ApplicationController
-  def index
-  end
+	def fetch
+		game = Game.find_by(width:params[:width], height:params[:height], mines:params[:mines])
+		# If there's no game matching that, this will return null. The front end should establish
+		# a websocket to await an available game.
+		render json: game.to_json
+	end
 end
--- rubymines/config/routes.rb
+++ rubymines/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-	get "/games", to: "games#index"
+	get "/game/:width/:height/:mines", to: "games#fetch"
 	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
